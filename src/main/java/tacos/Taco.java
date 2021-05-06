9713502257beaeb00f3f4e2a80df8f6a3a473dab
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-05-06 22:55:15 +0900

    feat : Validation add

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,11 +1,21 @@
 package tacos;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class Taco {
+
+    @NonNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<String> ingredients;
+    
 }
